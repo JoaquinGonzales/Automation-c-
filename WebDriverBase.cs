a640dfeaf033faac7ba276f2a2fb2ffb4b7b884e
Joaquin Gonzales <joaquin.gonzales@bolivia.assuresoft.com>
2019-08-02 18:33:49 -0400

    firstProject

---
+++ WebDriverBase.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingForAutomationClass
+{
+    public abstract class WebDriverBase
+    {
+        public IWebDriver driver;
+        public WebDriverBase()
+        {
+            driver = new ChromeDriver(@"C:\Users\joaquin.gonzales\Desktop\drivers");
+        }
+    }
+}
